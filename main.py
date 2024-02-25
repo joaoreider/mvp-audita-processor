2d86365171956eaec24005f2390aa61c3681c7d7
Joao Paulo <joaopauloj405@gmail.com>
2024-02-25 12:17:57 -0300

    feat: Add proccess analyze to file

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from flask import jsonify
 import boto3
 import os
 import pandas as pd
-from io import BytesIO
-from utils import normalize_md_file
+from io import BytesIO, StringIO
+import utils as helper
 
 BUCKET_NAME = 'audita-dev'
 
@@ -19,30 +19,29 @@ def get_files_from_folder_from_s3(bucket_name, folder_id, s3_instance):
     files.append(obj['Key'])
   return files
 
-def read_s3_file(bucket, key, s3_instance):
+def read_s3_excel_file(bucket, key, s3_instance):
     obj = s3_instance.get_object(Bucket=bucket, Key=key)
     content = obj['Body'].read()
     excel_file = BytesIO(content)
     df = pd.read_excel(excel_file)
     return df
 
+def read_s3_cmed_table_file(bucket, s3_instance):
+    obj = s3_instance.get_object(Bucket=bucket, Key="tabela-cmed/tabela_cmed_tratada.csv")
+    content = obj['Body'].read().decode('utf-8')
+    df = pd.read_csv(StringIO(content), sep=';')
+    return df
+
 def normalize_file(file):
   # treats and suits the files (trata e adéqua)
   try: 
-    return normalize_md_file(file)
+    return helper.normalize_md_file(file)
   except:
     # tentar analisar o arquivo de outra em outro padrão
+    app.logger.error(f"Error trying to normalize file {file}")
     return None
 
-def analyze_file(file):
-  # analyze and add data to the dataframe
-  pass
-
-
-def main(s3_folder_id):
-  if s3_folder_id:
-    start = time()
-    app.logger.info("Processor started!")
+def analyze_file(proposta, tabela_cmed):
 
     # Estrutura do  dataframe (resultado final do processamento de cada arquivo)
     resultado = {}
@@ -56,6 +55,121 @@ def main(s3_folder_id):
     descricao_da_proposta = []
     descricao_da_tabela = []
 
+    for row in proposta.iterrows():
+      registro_proposta = row[1]['registro']
+      marca_proposta = row[1]['marca']
+      apresentacao_proposta= row[1]['descricao']
+      preco_unitario_proposta = row[1]['preco_unitario']
+
+      registro_completo = len(str(registro_proposta)) == 13
+      # Campos da Tabela CMED
+      if registro_completo:
+          item_tabela_cmed = tabela_cmed[tabela_cmed['REGISTRO'] == str(registro_proposta)]
+      else:
+          item_tabela_cmed = tabela_cmed[tabela_cmed['REGISTRO'].apply(lambda x: str(registro_proposta) in str(x))]
+
+      achou_registro_proposta = len(item_tabela_cmed) > 0
+
+      registro.append(registro_proposta)
+      descricao_da_proposta.append(apresentacao_proposta)
+      preco_da_proposta.append(preco_unitario_proposta)
+      # Comparando os campos
+      if achou_registro_proposta: 
+          # print('Registro encontrado!')
+          marca_tabela_cmed = item_tabela_cmed['LABORATÓRIO'].values[0]
+          apresentacao_tabela_cmed = item_tabela_cmed['DESCRIÇÃO'].values[0]
+          preco_unitario_tabela_cmed = item_tabela_cmed['PF 18%'].values[0].replace(',', '.')
+          quantidade_tabela_cmed = item_tabela_cmed['quantidade'].values[0]
+          marca_valida = helper.confirmar_marca_do_registro(marca_proposta, marca_tabela_cmed)
+          descricao_da_tabela.append(apresentacao_tabela_cmed)
+          laboratorio.append(marca_tabela_cmed)
+          preco_da_tabela.append(preco_unitario_tabela_cmed)
+
+
+          if marca_valida:
+              # print('Marca válida!')
+              situacao_da_marca.append('OK')
+              if registro_completo:
+                  apresentacao_valida = helper.comparar_apresentacao_do_registro(apresentacao_proposta, apresentacao_tabela_cmed)
+
+                  if apresentacao_valida:
+                      # print('Apresentação válida!')
+                      preco_valido = helper.comparar_preco_unitario_do_registro(preco_unitario_proposta, preco_unitario_tabela_cmed)
+
+                      if preco_valido:
+                          # print('Preço válido!')
+                          situacao_do_preco.append('Abaixo')
+                          # print('Registro Aprovado!')
+                          situacao_registro.append('APROVADO')
+                      elif quantidade_tabela_cmed == 1:
+                          # print('Preço inválido!')
+                          situacao_do_preco.append('VERIFICAR')
+                          # print('Registro necessita de confirmação manual!')
+                          situacao_registro.append('VERIFICAR')
+                      else:
+                          # print('Preço inválido!')
+                          situacao_do_preco.append('Acima')
+                          situacao_registro.append('APROVADO')
+
+                  else:
+                      # print('Não foi possível comparar apresentação!')
+                      situacao_do_preco.append('VERIFICAR')
+                      # print('Registro necessita de confirmação manual!')
+                      situacao_registro.append('VERIFICAR')
+              else:
+                  situacao_do_preco.append('Inacessível')
+                  # print('Registro com 8 dígitos!')
+                  situacao_registro.append('APROVADO')
+          else:
+              # print('Não foi possível confirmar marca!')
+              situacao_da_marca.append('Inválida')
+              situacao_do_preco.append('Inacessível')
+              # print('Registro Reprovado!')
+              situacao_registro.append('REPROVADO')
+      else:
+          # print('Registro não encontrado!')
+          # print('Registro Reprovado!')
+          situacao_registro.append('REPROVADO')
+          situacao_da_marca.append('Inacessível')
+          situacao_do_preco.append('Inacessível')
+          descricao_da_tabela.append('Inacessível')
+          laboratorio.append('Inacessível')
+          preco_da_tabela.append('Inacessível')
+
+
+    # Monto o dataframe de resultados
+    resultado['registro'] = registro
+    resultado['situacao_registro'] = situacao_registro
+    resultado['laboratorio'] = laboratorio
+    resultado['situacao_da_marca'] = situacao_da_marca
+    resultado['situacao_do_preco'] = situacao_do_preco
+    resultado['preco_da_proposta'] = preco_da_proposta
+    resultado['preco_da_tabela'] = preco_da_tabela
+    resultado['descricao_da_proposta'] = descricao_da_proposta
+    resultado['descricao_da_tabela'] = descricao_da_tabela
+
+
+    try:
+
+      # for chave, valor in resultado.items():
+      #print(f'Chave: {chave}, tamanho: {len(valor)}')
+      df_final = pd.DataFrame(resultado)
+      return df_final
+
+    except: raise Exception('N foi possível montar o dataframe')
+
+
+def send_result_to_s3(bucket_name, path, s3_instance, result_dataframe):
+  # Send result to S3
+  csv_buffer = StringIO()
+  result_dataframe.to_csv(csv_buffer, index=False, sep=';')
+  s3_instance.put_object(Bucket=bucket_name, Key=f"{path}resultado.csv", Body=csv_buffer.getvalue())
+
+def main(s3_folder_id):
+  if s3_folder_id:
+    start = time()
+    app.logger.info("Processor started!")
+
 
     s3 = boto3.client('s3', region_name='us-east-1', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
     s3_files = get_files_from_folder_from_s3(bucket_name=BUCKET_NAME, folder_id=s3_folder_id, s3_instance=s3)
@@ -63,23 +177,24 @@ def main(s3_folder_id):
     dataframe_to_analyze = pd.DataFrame()
 
     for file in s3_files:
-
+      app.logger.debug(f"Processing file {file}")
       # lê o arquivo do S3
-      proposta = read_s3_file(BUCKET_NAME, file, s3_instance=s3)
+      proposta = read_s3_excel_file(BUCKET_NAME, file, s3_instance=s3)
 
       # trata e adéqua ao modelo padrão
       proposta_normalizada_no_formato_dataframe = normalize_file(proposta)
 
-      # concatena com o dataframe to analyze
-      # dataframe_to_analyze = pd.concat([dataframe_to_analyze, proposta_normalizada_no_formato_dataframe])
+      # dataframe to analyze: grupo de propostas normalizadas para analisar de uma vez
+      dataframe_to_analyze = pd.concat([dataframe_to_analyze, proposta_normalizada_no_formato_dataframe])
 
-    
-    
-    result_dataframe = analyze_file(file)
-    #TODO: Converter o dataframe final para JSON no formato do mock_response e retornar
-    with open('mock_response_large.json', 'r') as f:
-        data = json.load(f)
+    CMED_TABLE = read_s3_cmed_table_file(BUCKET_NAME, s3_instance=s3)
+    result_dataframe = analyze_file(dataframe_to_analyze, CMED_TABLE)
 
+    json_result = result_dataframe.to_json(orient='records')
+    # json string to json
+    data = json.loads(json_result)
+
+    send_result_to_s3(BUCKET_NAME, s3_folder_id, s3_instance=s3, result_dataframe=result_dataframe)
     end = time()
     app.logger.info(f"Processor finished in {round(end - start)} seconds")
     return data
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 
-from pandas import DataFrame 
+from app import app
+from unidecode import unidecode
+from difflib import SequenceMatcher
+from re import sub
+import pandas as pd
+import os
+
 def normalize_md_file(dataframe):
     # itens da proposta estão a partir da linha 23
     proposta = dataframe[23:]
@@ -21,7 +27,7 @@ def normalize_md_file(dataframe):
     dict_resultado['descricao'] = proposta_descricao_do_item
     dict_resultado['marca'] = proposta_marca
     dict_resultado['preco_unitario'] = proposta_preco
-    proposta = DataFrame(dict_resultado)
+    proposta = pd.DataFrame(dict_resultado)
 
     # Extrai só o registro da planilha de descrição do item
     temp = proposta['descricao'].apply(
@@ -33,8 +39,186 @@ def normalize_md_file(dataframe):
     temp_desc = proposta['descricao'].apply(
     lambda x: str(x).split('< Registro ANVISA :')[0])
     proposta['descricao'] = temp_desc
-  
     return proposta
 
 
+def confirmar_marca_do_registro(marca_proposta: str, marca_tabela: str) -> bool:
+    """
+    Compara a marca da proposta com a marca da tabela CMED
 
+    Parâmetros: 
+
+        marca_proposta (str): A marca do item que está na proposta.
+        marca_tabela (str): A marca do item que está na tabela.
+
+    Retorno:
+
+    (bool): True se a marca é a mesma False se não.
+
+    """
+    texto1 = unidecode(marca_proposta.upper())
+    texto2 = unidecode(marca_tabela.upper())
+
+    if texto1 in texto2:
+        return True
+
+    return False
+
+def comparar_apresentacao_do_registro(apresentacao_proposta: str, apresentacao_tabela: str) -> bool:
+
+    apresentacao_proposta_tratada = unidecode(_remover_caracteres(apresentacao_proposta).lower())
+    apresentacao_tabela_cmed_tratada = unidecode(_remover_caracteres(apresentacao_tabela).lower())
+    # Calcula a similaridade entre as strings de descrição
+    similarity_ratio = SequenceMatcher(None, apresentacao_proposta_tratada, apresentacao_tabela_cmed_tratada).ratio()
+    # print(f'COMPARAÇÃO:\n1: {apresentacao_proposta_tratada} \n2: {apresentacao_tabela_cmed_tratada}')
+    # print(f'Similaridade: {round(similarity_ratio, 2)}')
+    return round(similarity_ratio, 2) > 0.5
+
+def comparar_preco_unitario_do_registro(preco_proposta: float, preco_tabela: float) -> bool:
+    preco_tabela = float(str(preco_tabela).replace(',', '.'))
+    # print(f'COMPARAÇÃO:\nPreço proposta: {preco_proposta} \nPreço tabela: {preco_tabela}')
+
+    return preco_tabela >= preco_tabela
+
+def tratar_tabela_cmed(tabela_cmed):
+
+    # Cria uma coluna com a substância
+    tabela_cmed['DESCRIÇÃO'] = tabela_cmed['SUBSTÂNCIA'] + ' ' + tabela_cmed['APRESENTAÇÃO']
+    # Cria uma columa com a quantidade 
+    tabela_cmed['quantidade'] = _calcular_quantidade(tabela_cmed)
+    # Cria uma coluna com o preço unitário
+    tabela_cmed['PRECO_UNITARIO_BA'] = _calcular_preco_unitario(tabela_cmed)
+
+    tabela_cmed.to_csv(os.path.join("tabelas", "tabela_cmed_tratada.csv"), index=False, sep=';')
+
+    return tabela_cmed
+
+def _remover_caracteres(string):
+    # Expressão regular para pegar apenas letras, números ou o caractere "/"
+    padrao = r'[^A-Za-z0-9/]'
+    # Substitui os caracteres indesejados por uma string vazia
+    string_limpa = sub(padrao, '', string)
+    return string_limpa
+
+def _calcular_preco_unitario(tabela_cmed):
+    preco_unit = []
+    for i, preco in enumerate(tabela_cmed['PF 18%']):
+
+        quantidade = tabela_cmed['quantidade'].iloc[i]
+
+        if quantidade != -1:
+
+            try: 
+                aux = str(float(str(preco).replace(',', '.')) / int(quantidade)).replace('.', ',') #format(float(str(preco).replace(',', '.')) / int(quantidade), '.2f')
+                preco_unit.append(aux)
+
+            except:
+                preco_unit.append('indisponivel')
+        else:
+
+            preco_unit.append('indisponivel')
+
+    return preco_unit
+
+def _calcular_quantidade(tabela_cmed):
+    # coletando as quantidades do item de cada apresentacao
+    apresentacoes = tabela_cmed['APRESENTAÇÃO'].values
+    quant = []
+    for i in apresentacoes:
+        quant.append(_pegar_quantidade_da_apresentacao(i))
+    return quant
+
+def _pegar_quantidade_da_apresentacao(apresentacao: str) -> int:
+
+    """
+    Extrai a quantidade do item com base na apresentação.
+
+    Parâmetros: 
+
+        apresentacao (str): A apresentacao do item da tabela CMED.
+
+    Retorno:
+
+        (int): Quantidade extraída da apresentação. Se não achar retorna -1.
+
+    """
+
+    import re
+    texto = apresentacao.strip()
+
+    # Padrões da quantidade na apresentação
+    padrao_quantidade_no_meio_com_cx = re.compile(r"CX \d{1,3}")
+    padrao_quantidade_no_final = re.compile(r" X \d{1,3}$")
+    padrao_quantidade_no_meio_com_ct = re.compile(r"CT \d{1,3}")
+
+    # Matches (lista com o retorno do padrão encontrado)
+    matches_quantidade_no_meio_com_cx = padrao_quantidade_no_meio_com_cx.findall(texto)
+    matches_quantidade_no_final = padrao_quantidade_no_final.findall(texto)
+    matches_quantidade_no_meio_com_ct = padrao_quantidade_no_meio_com_ct.findall(texto)
+
+
+    # Quantidade é o valor do final multiplicado pelo do meio CT: Quantidade = "ct 10 x 2 " =  20
+    if len(matches_quantidade_no_final) and len(matches_quantidade_no_meio_com_ct):
+
+        #print(' # Quantidade é o valor do final multiplicado pelo do meio CT: Quantidade = "ct 10 x 2 " =  20')
+        quantidade_final = re.sub('[^0-9]', '', matches_quantidade_no_final[0])
+        quantidade_meio_ct =  re.sub('[^0-9]', '', matches_quantidade_no_meio_com_ct[0])
+
+        try: 
+            return int(quantidade_final) * int(quantidade_meio_ct)
+        except:
+            return -1
+    
+    # Quantidade é o valor do final multiplicado pelo do meio CX: Quantidade = "cx 10 x 2 " =  20
+    elif len(matches_quantidade_no_final) and len(matches_quantidade_no_meio_com_cx):
+        #print('  # Quantidade é o valor do final multiplicado pelo do meio CX: Quantidade = "cx 10 x 2 " =  20')
+        quantidade_final = re.sub('[^0-9]', '', matches_quantidade_no_final[0])
+        quantidade_meio_cx =  re.sub('[^0-9]', '', matches_quantidade_no_meio_com_cx[0])
+
+        try: 
+            return int(quantidade_final) * int(quantidade_meio_cx)
+        except:
+            return -1
+    
+    # Quantidade só no final
+    elif len(matches_quantidade_no_final) and not (len(matches_quantidade_no_meio_com_ct) or len(matches_quantidade_no_meio_com_cx)):
+
+        #print('# Quantidade só no final')
+        quantidade_final = re.sub('[^0-9]', '', matches_quantidade_no_final[0])
+
+        try: 
+            return int(quantidade_final)
+        except:
+            return -1
+    
+    # Quantidade só no meio com ct
+    elif not len(matches_quantidade_no_final) and (len(matches_quantidade_no_meio_com_ct)):
+        #print('# Quantidade só no meio com ct')
+        quantidade_meio_ct = re.sub('[^0-9]', '', matches_quantidade_no_meio_com_ct[0])
+
+        try: 
+            return int(quantidade_meio_ct)
+        except:
+            return -1
+    
+    # quantidade só no meio com cx
+    elif not len(matches_quantidade_no_final) and (len(matches_quantidade_no_meio_com_cx)):
+        
+        #print('# quantidade só no meio com cx')
+        #print(f' len qtd final: {len(matches_quantidade_no_final)}')
+        quantidade_meio_cx = re.sub('[^0-9]', '', matches_quantidade_no_meio_com_cx[0])
+
+        try: 
+            return int(quantidade_meio_cx)
+        except:
+            return -1
+    
+    # Quantidade não encontrada
+    else:
+        """
+        A quantidade não foi encontrada e por isso pode ser 1 ou não deu match na regex
+        Podemos considerar 1 porque se o preço tabelado for menor que o preço da proposta
+        o item será aprovado independente da quantidade, se o preço tabelado for maior que
+        o preço da proposta o item precisa de confirmação manual.
+        """
+        return 1 
